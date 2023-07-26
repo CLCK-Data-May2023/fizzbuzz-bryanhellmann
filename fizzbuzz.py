61d52e96ced14498ccd3131999a0d3ea8082e7a7
Bryan Hellmann <bryanhellmann@gmail.com>
2023-07-26 17:10:40 -0400

    Update fizzbuzz Python code to May 2023 HW repo

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
-# add your code here
+for i in range(1, 101):
+   #test if number is divisible by 3 & 5
+    if i % 3== 0 and i % 5 ==0:
+        print("FizzBuzz")
+    #Test if number is only divisible by 3
+    elif i % 3 == 0:
+        print ("Fizz")
+    #test if number is only divisble by 5
+    elif i % 5 == 0:
+        print("Buzz")
+    #prints all remaining numbers not meeting criteria above
+    else:
+        print(i)# add your code here
 
